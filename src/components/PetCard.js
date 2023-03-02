a758f4d8dad8c5f6617596dcc4008dd58d6ccf5a
shellyalmo <shellyalmo@gmail.com>
2023-03-02 12:51:03 +0200

    feat: add like button

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import heart from "../assets/filled-heart.png";
+import emptyHeart from "../assets/empty-heart.png";
 
 const PetCard = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <div>
-      <div>
+      <div className="card">
         <h3>בנגלי</h3>
         <img
-          className="card"
-          src="https://cdn2.thecatapi.com/images/ZocD-pQxd.jpg"
-          alt="cat"
+          className="like-btn"
+          src={active ? heart : emptyHeart}
+          alt="like button"
+          onClick={handleClick}
         />
+        <img src="https://cdn2.thecatapi.com/images/ZocD-pQxd.jpg" alt="cat" />
       </div>
     </div>
   );
