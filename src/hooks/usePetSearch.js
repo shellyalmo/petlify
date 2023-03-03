5ca2ff419d12351d0f744dcfcdd0307adda72146
shellyalmo <shellyalmo@gmail.com>
2023-03-03 14:57:55 +0200

    fix: add breed as query param

--- src/hooks/usePetSearch.js
+++ src/hooks/usePetSearch.js
@@ -33,7 +33,11 @@ function usePetSearch(species, breed) {
     const controller = new AbortController();
     dispatch({ type: ACTIONS.API_REQUESTS });
     (species === "cats" ? catsApi : dogsApi)
-      .get(`/images/search?breed_ids=${breed}&limit=10`, {
+      .get(`/images/search`, {
+        params: {
+          breed_ids: breed,
+          limit: 10,
+        },
         signal: controller.signal,
       })
       .then((res) => {
